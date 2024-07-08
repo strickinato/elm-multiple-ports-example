fe1498ec85bc8fea1ef3a17eeb493915f8f8a289
Aaron Strick <aaron.s@brilliant.org>
2024-07-08 11:23:08 -0700

    minimal

---
+++ Main.elm
@@ -0,0 +1,17 @@
+port module Main exposing (..)
+
+import Browser
+import Html exposing (Html)
+
+
+port hi : () -> Cmd msg
+
+
+main : Program () () ()
+main =
+    Browser.element
+        { init = \_ -> ( (), hi () )
+        , update = \_ model -> ( model, Cmd.none )
+        , view = \_ -> Html.text "other hi"
+        , subscriptions = \_ -> Sub.none
+        }
